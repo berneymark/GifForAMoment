8b5005efd040de003b6a15ba121865ce20645e06
Mark Berney <berneydwmark@gmail.com>
2020-07-22 18:01:16 +1200

    added trending tab to main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/Header/Header';
-import SearchBar from './Components/SearchBar/SearchBar'
+import SearchBar from './Components/SearchBar/SearchBar';
+import Trending from './Components/Trending/Trending';
 
 function App() {
   return (
