1822b264c5eab0c5fd20d8c85d1d0c6da0e9f763
Mark Berney <berneydwmark@gmail.com>
2020-07-23 11:58:14 +1200

    removed Giphy API from SearchBar component

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    componentDidMount() {
-        //const GIPHY_API_SEARCH_URL = 'https://api.giphy.com/v1/gifs/search';
-        const GIPHY_API_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random'
-        const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-
-        fetch(GIPHY_API_RANDOM_URL + '?api_key=' + GIPHY_API_KEY)
-            .then((response) => response.json())
-            .then((data) => console.log('This is your data: ', data))
-            .catch(function(error) {
-                console.log('There was an error.');
-            });
-    }
-
     render() {
         return (
             <div>
