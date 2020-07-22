1b534e483fa545b61acaa7077db97c6ed467ecd4
Mark Berney <berneydwmark@gmail.com>
2020-07-22 17:32:18 +1200

    Save trending results to state 'gifs'

--- src/Components/Trending/Trending.tsx
+++ src/Components/Trending/Trending.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class Trending extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            gifs : {data: []}
+        };
+    }
+
     componentDidMount() {
         const GIPHY_API_TRENDING_URL = 'https://api.giphy.com/v1/gifs/trending'
         const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
         fetch(GIPHY_API_TRENDING_URL + '?api_key=' + GIPHY_API_KEY)
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((data) => {
+                this.setState({
+                    gifs : data,
+                })
+            })
             .catch((error) => console.log('Error: ' + error));
     }
 
