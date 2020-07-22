e78fa3a88783797fe21adfbfcf4cd9052d6cfa51
Mark Berney <berneydwmark@gmail.com>
2020-07-22 14:47:57 +1200

    fetch Giphy api

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/Header/Header';
+import SearchBar from './Components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <SearchBar></SearchBar>
     </div>
   );
 }
--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -5,10 +5,21 @@ class SearchBar extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        const giphyAPIURL = 'api.giphy.com/v1/gifs/search';
+
+        fetch(giphyAPIURL)
+            .then((response) => response.json())
+            .then((data) => console.log('This is your data: '))
+            .catch(function(error) {
+                console.log('There was an error.');
+            });
+    }
+
     render() {
         return (
             <div>
-    
+                <h1>This component has loaded.</h1>
             </div>
         );
     }
