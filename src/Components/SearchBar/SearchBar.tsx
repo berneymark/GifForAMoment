00eb3468264b45b3af7cb77ec1271bbc653faed9
Mark Berney <berneydwmark@gmail.com>
2020-07-22 16:21:05 +1200

    working fetch CDM function

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    constructor(props: any) {
-        super(props);
-    }
-
     componentDidMount() {
-        const giphyAPIURL = 'api.giphy.com/v1/gifs/search';
+        const GIPHY_API_SEARCH_URL = 'https://api.giphy.com/v1/gifs/search';
+        const GIPHY_API_RANDOM_URL = 'https://api.giphy.com/v1/gifs/random'
+        const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
-        fetch(giphyAPIURL)
+        fetch(GIPHY_API_RANDOM_URL + '?api_key=' + GIPHY_API_KEY)
             .then((response) => response.json())
-            .then((data) => console.log('This is your data: '))
+            .then((data) => console.log('This is your data: ', data))
             .catch(function(error) {
                 console.log('There was an error.');
             });
