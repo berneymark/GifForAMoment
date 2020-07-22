0b143c52265562e7388eb2d07627bf1f3f630a25
Mark Berney <berneydwmark@gmail.com>
2020-07-22 17:28:53 +1200

    Trending fetch function

--- src/Components/Trending/Trending.tsx
+++ src/Components/Trending/Trending.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class Trending extends React.Component {
+    componentDidMount() {
+        const GIPHY_API_TRENDING_URL = 'https://api.giphy.com/v1/gifs/trending'
+        const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+        fetch(GIPHY_API_TRENDING_URL + '?api_key=' + GIPHY_API_KEY)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log('Error: ' + error));
+    }
+
     render() {
         return (
             <div>
