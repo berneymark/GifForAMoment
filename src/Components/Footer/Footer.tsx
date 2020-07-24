c38b1fa5c3bfb21c28c53bf1007c4d1cf75114fa
Mark Berney <berneydwmark@gmail.com>
2020-07-24 15:14:00 +1200

    added Giphy logo to footer and Mat-UI Typography

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+
+import GiphyLogo from '../../img/1024px-Giphy-logo.png'
+
+
 function Footer() {
     return (
         <div className="Footer">
-            <p>Created by Mark Berney</p>
-            <p>Powered by Giphy</p>
+            <Typography>Created by Mark Berney 2020. Powered by </Typography>
+            <img
+                src={GiphyLogo}
+                alt="Giphy Logo"/>
         </div>
     )
 }
 
 export default Footer;
+
