79bee407763199e693b9f340b6f5f577bdb6510a
Mark Berney <berneydwmark@gmail.com>
2020-07-24 18:56:44 +1200

    cleaned whitespace

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -4,17 +4,7 @@ import Box from '@material-ui/core/Box';
 
 import { makeStyles, Theme, createStyles } from '@material-ui/core';
 
-const useStyles = makeStyles((theme:Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1
-        }
-    })
-);
-
 function Results() {
-    const classes = useStyles();
-
     return (
         <Box border={1}>
 
