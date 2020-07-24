abbb1caa219a7941db67ac7e6cb5eaa44936e4c7
Mark Berney <berneydwmark@gmail.com>
2020-07-24 15:42:45 +1200

    fully designed Footer (w/ border)

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
 
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import GiphyLogo from '../../img/1024px-Giphy-logo.png'
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
 
+const useStyles = makeStyles((theme:Theme) =>
+    createStyles({
+        logo: {
+            height: 20,
+            paddingLeft: 10
+        }
+    })
+);
 
 function Footer() {
+    const classes = useStyles();
+
     return (
-        <div className="Footer">
-            <Typography>Created by Mark Berney 2020. Powered by </Typography>
-            <img
-                src={GiphyLogo}
-                alt="Giphy Logo"/>
-        </div>
+        <Box border={1}>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center">
+                <Grid item>
+                    <Typography variant="overline">
+                        Created by Mark Berney 2020. Powered by
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <img
+                        className={classes.logo}
+                        src={GiphyLogo}
+                        alt="Giphy Logo"/>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
