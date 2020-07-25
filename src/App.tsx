dbd8d445fe2fff477a7a30b7d924f3f4f8a72163
Mark Berney <berneydwmark@gmail.com>
2020-07-25 14:23:34 +1200

    reorder Giphy search link

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,12 @@ function App() {
         const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
         const GIPHY_API_RANDOM_TAGS = '&rating=g&tag='
 
-        const GIPHY_API_SEARCH_URL = 'https://api.giphy.com/v1/gifs/search?api_key='
-        const GIPHY_API_SEARCH_TAGS = '&limit=1&offset=0&rating=g&lang=en&q=';
+        const GIPHY_API_SEARCH_URL = 'https://api.giphy.com/v1/gifs/search?api_key=';
+        const GIPHY_API_SEARCH_RESULT = '&q=';
+        const GIPHY_API_SEARCH_TAGS = '&limit=1&offset=0&rating=pg&lang=en';
 
         if (searchTerm) {
-            fetch(GIPHY_API_SEARCH_URL + GIPHY_API_KEY + GIPHY_API_SEARCH_TAGS + searchTerm)
+            fetch(GIPHY_API_SEARCH_URL + GIPHY_API_KEY + GIPHY_API_SEARCH_RESULT + searchTerm + GIPHY_API_SEARCH_TAGS)
                 .then((response) => response.json())
                 .then((data) => {
                     setGifData(
@@ -59,7 +60,7 @@ function App() {
                 .catch((error) => console.log('Error: ' + error));
         }
 
-        console.log(gifData[0])
+        console.log(GIPHY_API_SEARCH_URL + GIPHY_API_KEY + GIPHY_API_SEARCH_RESULT + searchTerm + GIPHY_API_SEARCH_TAGS)
 
         return (
             <div>DONE</div>
